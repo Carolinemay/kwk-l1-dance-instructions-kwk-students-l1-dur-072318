8baa7aef9a0afb450c19c7d13aaac6f997cad8dd
Carolinemay <czhou@students.chccs.k12.nc.us>
2018-07-23 19:54:59 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,14 @@
 
 # Code your methods below
+def gangnum style
+  Assume the proper stance
+
+1. Plant legs far apart, bend knees slightly and keep posture loose
+
+Base Footwork
+
+1. Lift right foot
+
+2. Return right foot to the ground
+
+3. Finishing with a **small skip-step** backward
