904b8d81097660cc76e93eaa2c878a56daad400a
Carolinemay <czhou@students.chccs.k12.nc.us>
2018-07-23 20:01:24 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -41,3 +41,17 @@ upper body
 upper body
 upper body
 upper body
+def lasso
+  1. Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left.
+   - Lift right arm up and point right elbow diagonally to the right.
+   - Raise right forearm so that it points straight up, and **whip it in small circles** to the beat of the song, as if you were a cowboy holding a rope lasso.  
+ end
+ lasso
+ lasso
+ lasso
+ lasso
+ lasso
+ lasso
+ lasso
+ lasso
+ 
